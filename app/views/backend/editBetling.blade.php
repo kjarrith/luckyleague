229a7f39c0e590ba5f41ef4ba80036c829e25e9c
Kristján Ingi Geirsson <k.geirsson@gmail.com>
2017-03-19 20:33:15 +0000

    gleymdi $ merki

--- app/views/backend/editBetling.blade.php
+++ app/views/backend/editBetling.blade.php
@@ -2,9 +2,9 @@
 
 
 {{ Form::open(array('url' => 'editBetling', 'method' => 'post')) }}
-	<input type="text" name="betlingTitle" value="{{data->title}}">
+	<input type="text" name="betlingTitle" value="{{$data->title}}">
 
-	<input type="text" name="betlingOdds" value="{{data->odds}}">
+	<input type="text" name="betlingOdds" value="{{$data->odds}}">
 
 	<button type="submit">STAÐFESTA</button>
 {{ Form::close() }}
