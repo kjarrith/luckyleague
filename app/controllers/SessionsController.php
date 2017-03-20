7dd03c179176bd8b8d1d423d2cfd0a9b015c45ee
Kristján Ingi Geirsson <k.geirsson@gmail.com>
2017-03-20 12:39:56 +0000

    default img change

--- app/controllers/SessionsController.php
+++ app/controllers/SessionsController.php
@@ -59,7 +59,7 @@ class SessionsController extends \BaseController {
 		}
 		else if ($invites > 0)
 		{
-			$defaultImg = "http://vi.is/myndir/undirs%C3%AD%C3%B0ur/2014_10_17_verzlo_logo.png";
+			$defaultImg = "/img/verzlo_logo.png";
 
 			DB::insert('insert into users (profile_img,email,password,first_name,level,current_xp,current_balance, bet_limit) values (?,?,?,?,1,0,5000, 500)', array($defaultImg, $input['email'],$passwordhash,$input['firstname']));
 
